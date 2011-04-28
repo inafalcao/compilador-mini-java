79aa8c6896df88757734523008e49c54f1457772
danusarbc@gmail.com <danusarbc@gmail.com@e7faec5c-d53a-1f04-7fb7-1c6f791fc1a2>
2011-04-28 00:17:07 +0000

    com o menu escolhendo as opções de testes

--- trunk/src/compiladorMiniJava/compiladorMiniJava.java
+++ trunk/src/compiladorMiniJava/compiladorMiniJava.java
@@ -1,66 +1,56 @@
 package compiladorMiniJava;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.util.Scanner;
 
-import error.Error;
-
-import parser.MiniJavaParser;
 import parser.ParseException;
-import activationRegister.Frame;
-import activationRegister.Translate;
-import typeCheking.*;
-import visitor.SymbolTableVisitor;
 
 
-import symbolTable.*;
-import syntaxtree.Program;
+
+
 
 public class compiladorMiniJava {
 	
-	static File in;
-	static File out;
-	
-	
-	static MiniJavaParser parser;
-	static SymbolTable table;
-	static CheckType checkType;
-	static Frame frame;
-	static Translate translate;
-	
-	
 	public static void main(String[] args) throws ParseException{
-		
-		//reading file
-		try {
-			Reader in = new FileReader("files/Factorial.java");
+		Scanner input = new Scanner(System.in);
+			int op = 0;
+		do{
 			
-			//fase de parser
-			parser = new MiniJavaParser(in);
-			Program prog = parser.Program();
-			
-			//fase analise semantica
-			table = new SymbolTableVisitor().buildSymbolTable(prog);
-			SymbolTable.print();
-			
-			//fase verificacao de tipo
-			checkType = new CheckType(table);
-			checkType.visit(prog);
-			if(Error.getInstance().hasErro())Error.getInstance().print();
-			else{
-				System.out.println();
-				System.out.println("COMPILOU!");
-			}
-			
-			//fase traducao
-			
-			in.close();//close file
-		} catch (IOException e) {
-			System.out.println("Problemas para ler o arquivo");
-		}//ends catch
-		
+			System.out.println("BEM VINDO AO MINIJAVA COMPILADOR");
+			System.out.println("@AUTORES: CAMILA FERREIRA / DANUSA RIBEIRO/ LUCAS ASSUNÇÃO / PAULO ALVES");
+			System.out.println("O QUE DESEJA COMPILAR?");
+			System.out.println("1 -- FACTORIAL");
+			System.out.println("2 -- BINARY TREE");
+			System.out.println("3 -- BINARY SEARCH");
+			System.out.println("4 -- BUBBLE SHORT");
+			System.out.println("5 -- LINEAR SEARCH");
+			System.out.println("6 -- TREE VISITOR");
+			System.out.println("7 -- SAIR");
+			System.out.print(">> ");
+			op = input.nextInt();
+			switch(op){
+			case 1:
+				Factorial.factorial(); 
+				break;
+			case 2:
+				BinaryTree.binaryTree();
+				break;
+			case 3:
+				BinarySearch.binarySearch();
+				break;
+			case 4:
+				BubbleSort.bubbleSort();     
+				break;
+			case 5:
+				LinearTree.linearTree();	
+				break;
+			case 6:
+				TreeVisitor.treeVisitor();
+				break;
+			default:
+				System.out.println("VALOR INVALIDO"); 
+				break;
+			}//fim switch   
+		}while(op != 7);	
 	}//ends main
 
 }
